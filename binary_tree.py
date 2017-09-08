0f1a7cf986886ffe9c2c219667b5d68045a233ba
Ingrid  Spielman <ingridspielman@Ingrids-MacBook-Air-3.local>
2017-09-08 13:06:22 -0400

    add linked list

---
+++ binary_tree.py
@@ -0,0 +1,21 @@
+
+# median, mode, mean, find node, 
+
+
+
+
+class Node(object):
+
+	def __init__(self, parent=None):
+		self.parent=parent
+	
+	def add_child(self, child):
+		pass
+	def get_child(self, child):
+		pass
+
+		
+
+
+
+
---
+++ linked_list.py
@@ -0,0 +1,133 @@
+
+# single linked
+# Think of this as a dictionary, set, map
+class List(object):
+	def __init__(self, head=None):
+		self.head = head
+
+	def add_value(self, value):
+		n = Node(value)
+		if self.head is not None:
+			n.next = self.head	
+			self.head = n
+		else:
+			self.head = n
+
+	def add_to_tail(self, value):
+		h = self.head
+		if h is None:
+			self.add_value(value)	
+		else:			
+			while h.next is not None:
+				h = h.next
+			n = Node(value)	
+			h.insert_node(n)
+
+	def remove_value(self, value):
+		h = self.head
+
+		# head case
+		if h.value == value:
+			self.head = h.next
+
+		while h is not None:
+			# middle/tail case
+			if h.next is not None:
+				if h.next.value == value:
+					h.next = h.next.next
+					return
+			h = h.next
+
+	def reverse(self):
+		h = self.head
+		while h is not None:
+			end = h.value
+			self.remove_value(h.value)
+			self.add_value(end)
+			h = h.next		
+
+	def reverse_pointers(self):
+		h = self.head
+		count = 0
+		while h is not None:
+			a = h
+			b = h.next 
+			# first value
+			if count == 0:
+				a.next = None
+			# last value
+			if h.next is None:
+				self.head = h
+			a.next = None
+			b.next = first
+			h = h.next
+
+
+	def contains_value(self,value):
+		h = self.head
+		while h is not None:
+			if h.value == value:
+				return True
+			h = h.next		
+		return False
+
+	def value_nth_from_end(self, n):
+		count = 0
+		h = self.head
+		while h is not None:
+			count += 1
+			h = h.next	
+
+		nth_from_end = count - n
+		if nth_from_end < 0:
+			return None
+		h = self.head
+		new_count = 0
+		while h is not None:
+			if new_count == nth_from_end:
+				return h.value
+			new_count += 1
+			h = h.next	
+
+	def print_list(self):
+		h = self.head
+		if h is None:
+			print "None"
+		else:
+			s = str(h.value)
+			while h.next is not None:
+				h = h.next
+				s += " --> " + str(h.value)
+			print s
+
+
+class Node(object):
+
+	def __init__(self, value=None, next_node=None):
+		self.value=value
+		self.next=next_node
+
+	def insert_node(self, node):
+		if self.next is None:
+			self.next=node
+		else:
+			node.next = self.next
+			self.next=node
+
+
+
+l = List()
+l.add_value(0)
+l.add_value(1)
+l.add_value(2)
+l.add_value(5)
+l.add_value(4)
+l.add_value(3)
+l.print_list()
+l.reverse()
+l.print_list() # 1
+l.reverse_pointers()
+l.print_list() # 1
+
+
+		
