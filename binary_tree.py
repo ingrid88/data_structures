c70341b7c6ada24efda51c6f5c061861a85f3c3a
Ingrid Spielman <ingspi@gmail.com>
2017-09-11 12:14:50 -0400

    start binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -1,13 +1,35 @@
 
-# median, mode, mean, find node, 
+# median, mode, mean, find node, reorder, p
 
 
+class Tree(object):
+
+	def __init__(self, head=None):
+		head = head
+
+	def add_node(self, value=value, child=child, parent=parent):
+		child = Node(value)
+		if parent.children not None:
+			parent.children.append(child)
+		else:
+			parent.children = [child]
+
+	def print_tree(self):
+		pass
+
+	def depth_first_search(self, value):
+		pass
+
+	def breadth_first_search(self, value):
+		pass
 
 
 class Node(object):
 
-	def __init__(self, parent=None):
-		self.parent=parent
+	def __init__(self, value=value, parent=None, children=None):
+		self.value = value
+		self.parent = parent
+		self.children = children
 	
 	def add_child(self, child):
 		pass
@@ -15,6 +37,10 @@ class Node(object):
 		pass
 
 		
+
+t = Tree()
+h = Node(3)
+t.head = h
 
 
 
