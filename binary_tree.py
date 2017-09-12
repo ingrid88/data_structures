8148d9bfedf0f3b15f651be055ffb8cf6ebea6cc
Ingrid Spielman <ingspi@gmail.com>
2017-09-12 11:43:36 -0400

    refactor print

--- binary_tree.py
+++ binary_tree.py
@@ -9,6 +9,7 @@
 #. / \   / \
 # 9   3 2   1
 import pdb
+import math
 
 class Tree(object):
 
@@ -41,6 +42,48 @@ class Tree(object):
 						nodes.append(children[1])
 
 
+	def number_of_rows(self, row_values=[], count=1):
+		new_row_values = []
+		for node in row_values:
+			if node.children is not None:
+				for child in node.children:
+					new_row_values.append(child)
+
+		if len(new_row_values) == 0:
+			return count
+		return self.number_of_rows(new_row_values, count+1)
+
+
+	def max_depth_number_of_rows(self, node, count=0):
+		# gets the max depth path from a specific node
+		# return depth from specified node
+		if node.children is None:
+			return count
+		else:
+			first_child = node.children[0] if node.children[0] is not None else None
+			second_child = node.children[1] if node.children[1] is not None else None
+			return max(
+				self.max_depth_number_of_rows(first_child, count + 1), 
+				self.max_depth_number_of_rows(second_child, count + 1)
+			)
+		pass
+
+
+
+	def print_tree_recursively(self, row_values=[], count=1):
+		new_row_values = []
+		s = ''
+		for node in row_values:
+			s += str(node.value)+" "
+			if node.children is not None:
+				for child in node.children:
+					new_row_values.append(child)
+		print s
+		if len(new_row_values) == 0:
+			return 
+		return self.print_tree_recursively(new_row_values, count+1)
+
+
 	def print_tree(self):
 		nodes = [self.head]
 		values = [self.head.value]
@@ -52,10 +95,17 @@ class Tree(object):
 					if child is not None:
 						nodes.append(child) 
 						values.append(child.value)
-			power = 2**count
-			count += 1		
-			print (" "*count).join([str(x) for x in values[0:power]])
-			values = values[power::]
+			count += 1	
+		print values
+		print len(values)	
+		rows = int(math.floor(math.log(len(values)-1,2))+1)
+		print "there are {} rows".format(rows)
+		for i in range(rows):
+			# print "row # is {}".format(i)
+			power = 2**i
+			# pdb.set_trace()
+			print " ".join(str(x) for x in values[:power])
+			values = values[power:]
 
 
 	def breadth_first_search(self, value):
@@ -111,4 +161,33 @@ t.add_node(1)
 t.add_node(8)
 t.add_node(3)
 t.add_node(13)
-t.print_tree()
+t.add_node(3)
+t.add_node(2)
+t.add_node(100)
+t.add_node(8)
+t.add_node(3)
+t.add_node(3)
+t.add_node(2)
+t.add_node(1)
+t.add_node(8)
+t.add_node(3)
+t.add_node(3)
+# t.print_tree()
+t.print_tree_recursively([t.head])
+
+print "there are {} rows".format(t.number_of_rows([t.head]))
+print t.max_depth_number_of_rows(t.head)
+
+
+	# def calculate_rows(self):		
+	# 	nodes = [self.head]
+	# 	values = [self.head.value]
+	# 	count = 0
+	# 	while len(nodes) > 0:
+	# 		parent = nodes.pop(0)
+	# 		if parent.children is not None:
+	# 			for child in parent.children:
+	# 				if child is not None:
+	# 					nodes.append(child) 
+	# 					values.append(child.value)
+	# 	return math.floor(math.log(len(values)-1,2))+1
