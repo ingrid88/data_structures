364e427db8c8ca4ce235f7de52e09162bd451f50
Ingrid Spielman <ingspi@gmail.com>
2017-09-28 15:51:53 -0400

    add more methods

--- arrays.py
+++ arrays.py
@@ -126,9 +126,148 @@ print print_combos(s)
 # print "{} variants found".format(number_of_variants_found(s, words))
 # print words_found(s)
 # print words_found2(s)
+#####################################
+def find_grants_cap(grantsArray, newBudget):
+  # algorithm: 
+    # { For all values in array that are less than the cap/len(array)
+      # substract that value from the cap and count the number of times this occurs
+    # divide the new value by the number of numbers over the cap/len(array) 
+    # replace all values with new value calculated in division }
+  count = 0
+  for grant in grantsArray:
+    if grant < float(newBudget) / len(grantsArray):
+      count += 1
+      newBudget -= grant
+  print newBudget
+  for i, grant in enumerate(grantsArray):
+    if grant > float(newBudget) / len(grantsArray):
+      grantsArray[i] = float(newBudget) / (len(grantsArray) - count)
+  cap = float(newBudget) / (len(grantsArray) - count)
+  return cap
 
+import numpy
+
+def find_grants_cap(grantsArray, newBudget):
+  # algorithm: 
+    # { Order grants from least to greatest and iterate through
+      # if the value is less than the average_cap
+          # substract value from budget and recaculate cap
+      # if the value is more than the cap, then return cap }
+  grantsArray = numpy.sort(grantsArray)
+  cap = float(newBudget) / len(grantsArray)
+  for i, grant in enumerate(grantsArray):
+    if grant < cap:
+      newBudget -= grant
+      cap = float(newBudget) / len(grantsArray[i+1:])
+  return cap
+
+grantsArray = [2, 100, 50, 120, 1000]
+newBudget = 190
+print find_grants_cap(grantsArray, newBudget)
+
+grantsArray = [2,100,50,120,167]
+newBudget = 400
+print find_grants_cap(grantsArray, newBudget)
+# output: 47 # and given this cap the new grants array would be
+           # [2, 47, 47, 47, 47]. Notice that the sum of the
+           # new grants is indeed 190
 
 ##########################
+
+import random
+
+
+def inplace_shuffle(l):
+    for i, value in enumerate(l):
+        random_position = numpy.floor(len(l[i:])*random.random() + i)
+        b = l[random_position]
+        a = value
+        l[i] = b
+        l[random_position] = a
+    return l
+
+# l = [1,5,2,4,3]
+# print inplace_shuffle(l)
+# Write a function for doing an in-place shuffle of a list.
+# The shuffle must be "uniform," meaning each item in the original 
+# list must have the same probability of ending up in each spot in the final list.
+
+# Assume that you have a function get_random(floor, ceiling) 
+# for getting a random integer that is >= floor and <= ceiling.    
+#########################
+
+def twoSum(nums, target):
+    seq_length = len(nums)
+    for i in range(seq_length):
+        for j in range(seq_length):
+            if (nums[i] != nums[j]) and (nums[j] + nums[i] == target):
+                print nums[i]
+                print nums[j]
+                print nums[i] + nums[j]
+                return [i, j]
+    return None   
+
+print "twoSum"
+print twoSum([2,3,7,5], 12)  
+print "  "   
+##########################
+
+    def addTwoNumbers(l1, l2):
+        carry_over = 0
+        solution = []
+        print l1
+        print l2
+        i = 0
+        diff = len(l1) - len(l2)
+        if diff > 0:
+            solution = l1[0:diff]
+            l1 = l1[diff:]
+        if diff < 0:
+            solution = l2[0:diff]
+            l2 = l2[diff:]
+        for i in range(len(l1)-diff):
+            s = l1[i] + l2[i]
+            left_over = s % 10
+            solution.append(left_over + carry_over)
+            carry_over = 10 - left_over
+        return solution
+
+##########################
+
+
+def find_duplicates(arr1, arr2):
+  duplicates = []
+  i = 0
+  j = 0
+  while i < len(arr1) and j < len(arr2):
+    if arr1[i] < arr2[j]:
+      i += 1
+    elif arr1[i] > arr2[j]:
+      j += 1
+    else:
+      duplicates.append(arr1[i])
+      j += 1
+      i += 1
+  return duplicates
+
+arr1 = [1, 2, 3, 5, 6, 7]
+
+arr2 = [3, 6, 7, 8, 20]
+print find_duplicates(arr1, arr2)
+'''
+ arr1 = [1, 2, 3 ...1000... 2000] len(arr1) = 10
+                    i
+ arr2 = [1000 --- 1004] ... len(arr2) = 10000000000
+            j
+            
+            
+   M = very large  
+   
+   N = small each value from the smaller array, in the bigger array.
+   N^2 + N 
+ 
+'''
+
 
 # If a=1, b=2, c=3,....z=26. Given a string, find all possible codes that string can generate. Give a count as well as print the strings.
 
@@ -142,6 +281,8 @@ print print_combos(s)
 # aaw // a= 1, a =1, w= 23
 # kw // k = 11, w = 23
 
+def generated_strings(string_number="1123"):
+    pass
 
 ###############################
 
