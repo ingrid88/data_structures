bc92a3e04691d351699011cbdae9ae4441f38cb4
Ingrid Spielman <ingspi@gmail.com>
2017-10-10 00:22:44 -0400

    add napsack

---
+++ nap_sack.py
@@ -0,0 +1,13 @@
+
+
+
+
+def napsack(bag_size, objects):
+	# fill bag as much as possible without going over its size (can't reuse the objects)
+	pass
+
+
+def make_change(change, coin_denominations=[1,5,10,25]):
+	# make change with as few coins as you can given
+	pass 
+
